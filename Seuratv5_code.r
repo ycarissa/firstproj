366724bcde1e565668423dbe66acefecbf435db3
Carissa Yun <carissa.yun@gmail.com>
2025-07-25 19:50:07 -0500

    7/25/25 - UMAP Generation

--- Seuratv5_code.r
+++ Seuratv5_code.r
@@ -1,7 +1,9 @@
 library(Seurat)
-library (ggplot2)
+library(ggplot2)
+library(glmGamPoi)
+library(dplyr)
 
-#### Single Cell RNAseq Analysis based on the GEO database dataset GSE292807 ####
+#### Single Cell RNAseq Analysis based on the GEO database dataset GSE282630 ####
 ## Comparison of WT to NOTCH2 gain-of-function mice ##
 
 
@@ -15,16 +17,16 @@ dir.create(save_dir)
 counts_het <- Read10X(data.dir = paste0(work_dir, "/Het_NOTCH"))
 counts_wt  <- Read10X(data.dir = paste0(work_dir, "/WT_NOTCH"))
 
-Het.s <- CreateSeuratObject(counts = counts_het, min.cells = 3, min.features = 200)
-WT.s  <- CreateSeuratObject(counts = counts_wt,  min.cells = 3, min.features = 200)
+Het.s <- CreateSeuratObject(counts = counts_het, project = "Het", min.cells = 3, min.features = 200)
+WT.s  <- CreateSeuratObject(counts = counts_wt,  project = "WT", min.cells = 3, min.features = 200)
 
 
 ### QC filtering
 qc_dir <- paste0(save_dir, "/QC_filtering")
 dir.create(qc_dir)
 
-Het.s[["percent.mt"]] <- PercentageFeatureSet(Het.s, pattern = "^MT-")
-WT.s[["percent.mt"]] <- PercentageFeatureSet(WT.s, pattern = "^MT-")
+Het.s[["percent.mt"]] <- PercentageFeatureSet(Het.s, pattern = "^mt-")
+WT.s[["percent.mt"]] <- PercentageFeatureSet(WT.s, pattern = "^mt-")
 
 #Violin plots for filtering for Seurat Object Het.s
 p1 <- VlnPlot(Het.s, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3) + labs(x = "Identity", y = "Expression Level")
@@ -45,10 +47,32 @@ p4 <- p2 + p3
 ggsave("QC_violin_WT.png", plot = p1, path = qc_dir, width = 10, height = 8, dpi = 600)
 ggsave("QC_scatter_WT.png", plot = p4, path = qc_dir, width = 10, height = 8, dpi = 600)
 
-### Subset based on Cells with >250 nFeature, >10% percent mt and >10,000 genes/cell based on original analysis
-Het_subset.s <- subset(Het.s, subset = nFeature_RNA > 250 & nFeature_RNA < 10000 & nCount_RNA < 10000 & percent.mt < 5)
-WT_subset.s <- subset(WT.s, subset = nFeature_RNA > 250 & nFeature_RNA < 10000 & nCount_RNA < 10000 & percent.mt < 5)
+### Subset based on Cells with 15000 > nFeature > 250, >10% percent mt and 15000 > nCount.
+Het_subset.s <- subset(Het.s, subset = nFeature_RNA > 250 & nFeature_RNA < 10000 & nCount_RNA < 15000 & percent.mt < 10)
+WT_subset.s <- subset(WT.s, subset = nFeature_RNA > 250 & nFeature_RNA < 10000 & nCount_RNA < 15000 & percent.mt < 10)
+
+### Integration
+combined.s <- merge(x = Het_subset.s, y = WT_subset.s, project = "combined", add.cell.ids = c("Het", "WT"))
+Idents(combined.s) <- "orig.ident"
+
+combined.s <- SCTransform(combined.s, vars.to.regress = "percent.mt", verbose = FALSE)
+combined.s <- RunPCA(combined.s, assay = "SCT", verbose = FALSE)
+ElbowPlot(combined.s) ### 1:10 was chosen
 
 
+combined.s <- IntegrateLayers(object = combined.s, method = CCAIntegration, assay = "SCT", normalization.method = "SCT", orig.reduction = "pca", new.reduction = "integrated.cca", verbose = FALSE)
 
+combined.s <- RunUMAP(combined.s, reduction = "integrated.cca", dims = 1:10)
+combined.s <- FindNeighbors(combined.s, reduction = "integrated.cca", dims = 1:10)
+combined.s <- FindClusters(combined.s, resolution = 0.5)
+DimPlot(combined.s, reduction = "umap")
+
+###Saving plots
+u1 <- DimPlot(combined.s, reduction = "umap", label = TRUE, group.by = "seurat_clusters") +
+  labs(title = "UMAP by Clusters")
+u2 <- DimPlot(combined.s, reduction = "umap", label = TRUE, group.by = "orig.ident") +
+  labs(title = "UMAP by Identity")
+
+ggsave(filename = file.path(save_dir, "UMAP_by_Clusters.png"), plot = u1, width = 8, height = 6, dpi=600)
+ggsave(filename = file.path(save_dir, "UMAP_by_Identity.png"), plot = u2, width = 8, height = 6, dpi=600)
 
